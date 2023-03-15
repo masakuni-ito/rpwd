aa1f85a006860f02328523fa041f2e8628c13ec9
Masakuni Ito <masakuni.ito.19@gmail.com>
2023-03-15 23:41:38 +0900

    use struct with color attribute for dirs

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,40 @@ const STYLES: [Colour; 5] = [
     Colour::Blue,
     Colour::Purple,
 ];
+struct ColorizedPath {
+    path: String,
+    color: Colour,
+}
+
+impl ColorizedPath {
+    fn new(path: String, color: Colour) -> ColorizedPath {
+        ColorizedPath {
+            path: path,
+            color: color,
+        }
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let current_dir = env::current_dir().unwrap();
-
-    let mut dirs = vec![];
-    for component in current_dir.components() {
-        if let std::path::Component::Normal(dir) = component {
-            dirs.push(dir.to_string_lossy());
-        }
-    }
+    let mut dirs: Vec<ColorizedPath> = vec![];
 
     let mut style_count = 0;
-    let mut styled_dirs = vec![];
-    for dir in dirs {
-        let style = STYLES.get(style_count).unwrap().paint(dir);
-        styled_dirs.push(style.clone());
+    for component in current_dir.components() {
+        if let std::path::Component::Normal(dir) = component {
+            let style = STYLES.get(style_count).unwrap();
+            let dir = ColorizedPath::new(dir.to_string_lossy().to_string(), style.clone());
+            dirs.push(dir);
 
-        style_count += 1;
-        if style_count >= STYLES.len() {
-            style_count = 0;
+            style_count += 1;
+            if style_count >= STYLES.len() {
+                style_count = 0;
+            }
         }
     }
 
-    for dir in styled_dirs {
-        print!("/{}", dir.to_string());
+    for dir in dirs {
+        print!("/{}", dir.color.paint(dir.path));
     }
     print!("\n");
 
