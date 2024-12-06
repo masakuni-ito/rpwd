52d746ee52e713dcaf5603302b725bae4febf775
Masakuni Ito <masakuni.ito.19@gmail.com>
2024-12-06 19:47:13 +0900

    Refactor run function to transform PathBuf into a string array
    - Converted `PathBuf` components into a `Vec<String>` for easier manipulation.
    - Updated the concatenation logic to use `join` on the string array for constructing the path.
    - Improved readability and flexibility by separating the conversion and concatenation steps.
    - Removed unnecessary calls to `skip(1)` to keep all path components intact.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ use std::path::{PathBuf, MAIN_SEPARATOR_STR};
 fn run() -> Result<bool, io::Error> {
     let current_dir: PathBuf = env::current_dir()?;
 
-    let concatenated_path: String = current_dir
+        // PathBuf を文字列の配列に変換
+    let path_components: Vec<String> = current_dir
         .iter()
         .skip(1)
-        .map(|component| component.to_string_lossy())
-        .collect::<Vec<_>>()
-        .join(MAIN_SEPARATOR_STR);
+        .map(|component| component.to_string_lossy().to_string()) // 各コンポーネントを String に変換
+        .collect();
+
+    let concatenated_path: String = path_components.join(MAIN_SEPARATOR_STR);
 
     println!("{}{}", MAIN_SEPARATOR_STR, concatenated_path);
     Ok(true)
